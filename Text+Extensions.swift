ca9ac0123ec20d21c544ee1684eca2705a5eb481
Victor Chen <victor.chen@mtzendo.com>
2023-01-08 22:00:58 +1300

    add more extensions

---
+++ Text+Extensions.swift
@@ -0,0 +1,21 @@
+
+
+import SwiftUI
+
+extension Text {
+
+    public func foregroundLinearGradient(colors: [Color], startPoint: UnitPoint, endPoint: UnitPoint) -> some View {
+        self.overlay {
+            LinearGradient(
+                colors: colors,
+                startPoint: startPoint,
+                endPoint: endPoint
+            )
+            .mask(
+                self
+
+            )
+        }
+    }
+
+}
